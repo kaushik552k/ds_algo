9f59c2664a85cb3ce826b727f06d50c98d5bf64a
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-08-02 01:29:47 +0530

    Created linear search

---
+++ src/linear_search.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class linear_search {
+    public static void main(String[] args){
+        Scanner sc = new Scanner( System.in );
+        int[] a = {12, 4, 7, 23, 66, 45};
+        System.out.println( Arrays.toString( a ) );
+        System.out.println( "Enter the value you want to search" );
+        int search_value = sc.nextInt();
+        int search_index = linearSearch( a, search_value );
+        if(search_index == -1)
+            System.out.println( "not found" );
+        else
+            System.out.println( search_index );
+    }
+
+    private static int linearSearch(int[] a, int search_value){
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] == search_value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
